f707810c1b0af3766a2e9c1689c6b4112d43ea7c
EduRosa96 <eduardororo96@gmail.com>
2024-04-05 18:58:41 +0200

    subida segunda

--- src/main/java/EditorTexto/EditorTextoERR.java
+++ src/main/java/EditorTexto/EditorTextoERR.java
@@ -1,2 +1,20 @@
-package EditorTexto;public class EditorTextoERR {
+package EditorTexto;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class EditorTextoERR extends JFrame implements ActionListener {
+
+    private JPanel panel1;
+    private JTextField textField1;
+
+    public EditorTextoERR() {
+         panel1 = new JPanel();
+         panel1.setLayout(null);
+         getContentPane().add(panel1);
+         panel1.setBackground(new java.awt.Color(68, 244, 131));
+         add(panel1);
+
+    }
 }
